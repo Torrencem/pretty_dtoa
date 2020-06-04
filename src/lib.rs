4363a752d9c9a17998f51b75a07d64825c3d395b
Torrencem <matt@torrencefamily.net>
2020-06-04 14:30:21 -0400

    refactor, better comments, slight order of options change

--- src/lib.rs
+++ src/lib.rs
@@ -226,114 +226,118 @@ const fn digit_to_u8(val: u8) -> u8 {
     val + '0' as u8
 }
 
-fn digits_to_a(sign: bool, mut s: Vec<u8>, mut e: i32, config: FmtFloatConfig) -> String {
-    if config.ignore_extremes.is_some() || config.max_sig_digits.is_some() {
+fn digits_to_a(sign: bool, mut digits: Vec<u8>, mut e: i32, config: FmtFloatConfig) -> String {
+    // The main string formatting function. digits is a vector of the digits
+    // found using the ryu backend function. The value of the float is
+    // <- if sign>0.<digits> * 10^<e>
+    // NOTE: digits is ascii, so the digit "5" would be represented as "digit_to_u8(5)"
+    if let Some(limit) = config.max_sig_digits {
+        // Remove extra significant digits
+        let limit = limit as usize;
+        if digits.len() > limit {
+            let removed = digits.drain(limit..).next().unwrap();
+            if config.round_mode == RoundMode::Round && removed >= digit_to_u8(5) {
+                // round up
+                let mut l = digits.len() - 1;
+                digits[l] += 1;
+                while digits[l] == digit_to_u8(10) {
+                    if l == 0 {
+                        digits[0] = digit_to_u8(1);
+                        e += 1;
+                        break;
+                    }
+                    digits.pop();
+                    l -= 1;
+                    digits[l] += 1;
+                }
+            }
+        }
+    }
+    if let Some(limit) = config.max_decimal_digits {
+        // Remove extra decimal digits
+        let adjusted_limit_position = limit as i32 + e;
+        if (0 <= adjusted_limit_position) && (adjusted_limit_position < digits.len() as i32) {
+            let final_char = digits.drain(adjusted_limit_position as usize ..).nth(0).unwrap();
+            if config.round_mode == RoundMode::Round && final_char >= digit_to_u8(5) {
+                // round up
+                let mut l = digits.len() - 1;
+                digits[l] += 1;
+                while digits[l] == digit_to_u8(10) {
+                    if l == 0 {
+                        digits[0] = digit_to_u8(1);
+                        e += 1;
+                        break;
+                    }
+                    digits.pop();
+                    l -= 1;
+                    digits[l] += 1;
+                }
+            }
+        }
+    }
+    if let Some(limit) = config.ignore_extremes {
+        // Ignore <limit> consecutive 9's or 0's. A copy of digits is made
         let mut stripped_string: Vec<u8> = Vec::with_capacity(30);
         let mut nine_counter = 0;
         let mut zero_counter = 0;
-        let mut curr = 0;
-        for digit in s.iter() {
-            if let Some(limit) = config.ignore_extremes {
-                if *digit != digit_to_u8(9) {
-                    nine_counter = 0;
-                } else {
-                    nine_counter += 1;
-                    if nine_counter >= limit {
-                        // 14999...
-                        stripped_string.drain((stripped_string.len() + 1 - nine_counter as usize)..);
-                        // -> 14
-                        let l = stripped_string.len();
-                        if l == 0 {
-                            // for strings like 999
-                            // 999e3 -> 1e4
-                            stripped_string.push(digit_to_u8(1));
-                            e += 1;
-                        } else {
-                            // Rounding doesn't have to happen here, because what was removed
-                            // was exactly limit 9's
-                            // 15
-                            stripped_string[l - 1] += 1;
-                        }
-                        break;
-                    }
-                }
-
-                if *digit != digit_to_u8(0) {
-                    zero_counter = 0;
-                } else {
-                    zero_counter += 1;
-                    if zero_counter >= limit {
-                        // 14000...
-                        stripped_string.drain((stripped_string.len() + 1 - zero_counter as usize)..);
-                        // 14
-                        break;
-                    }
-                }
-            }
-            if let Some(limit) = config.max_sig_digits {
-                if curr + 1 > limit {
-                    if *digit >= digit_to_u8(5) && config.round_mode == RoundMode::Round {
-                        // round up
-                        let mut l = stripped_string.len() - 1;
-                        stripped_string[l] += 1;
-                        while stripped_string[l] == digit_to_u8(10) {
-                            if l == 0 {
-                                stripped_string[0] = digit_to_u8(1);
-                                e += 1;
-                                break;
-                            }
-                            stripped_string.pop();
-                            l -= 1;
-                            stripped_string[l] += 1;
-                        }
+        for digit in digits.iter() {
+            if *digit != digit_to_u8(9) {
+                nine_counter = 0;
+            } else {
+                nine_counter += 1;
+                if nine_counter >= limit {
+                    // 14999...
+                    stripped_string.drain((stripped_string.len() + 1 - nine_counter as usize)..);
+                    // -> 14
+                    let l = stripped_string.len();
+                    if l == 0 {
+                        // for strings like 999
+                        // 999e3 -> 1e4
+                        stripped_string.push(digit_to_u8(1));
+                        e += 1;
+                    } else {
+                        // Rounding doesn't have to happen here, because what was removed
+                        // was exactly limit 9's
+                        // 15
+                        stripped_string[l - 1] += 1;
                     }
                     break;
                 }
             }
-            curr += 1;
+
+            if *digit != digit_to_u8(0) {
+                zero_counter = 0;
+            } else {
+                zero_counter += 1;
+                if zero_counter >= limit {
+                    // 14000...
+                    stripped_string.drain((stripped_string.len() + 1 - zero_counter as usize)..);
+                    // 14
+                    break;
+                }
+            }
             stripped_string.push(*digit);
         }
-        s = stripped_string;
+        digits = stripped_string;
     }
     if let Some(limit) = config.min_sig_digits {
-        // 123 -> 12300
-        // (min_sig_digits = 5)
-        let mut curr = s.len() as u8;
+        // Pad 0's to get enough significant digits
+        let mut curr = digits.len() as u8;
         while curr < limit {
-            s.push(digit_to_u8(0));
+            digits.push(digit_to_u8(0));
             curr += 1;
         }
     }
     if let Some(limit) = config.min_decimal_digits {
-        // 1230 -> 1230.00
+        // Pad 0's to get enough decimal digits
         let adjusted_limit_position = limit as i32 + e;
-        while (s.len() as i32) < adjusted_limit_position {
-            s.push(digit_to_u8(0));
-        }
-    }
-    if let Some(limit) = config.max_decimal_digits {
-        let adjusted_limit_position = limit as i32 + e;
-        if (0 <= adjusted_limit_position) && (adjusted_limit_position < s.len() as i32) {
-            let final_char = s.drain(adjusted_limit_position as usize ..).nth(0).unwrap();
-            if config.round_mode == RoundMode::Round && final_char >= digit_to_u8(5) {
-                // round up
-                let mut l = s.len() - 1;
-                s[l] += 1;
-                while s[l] == digit_to_u8(10) {
-                    if l == 0 {
-                        s[0] = digit_to_u8(1);
-                        e += 1;
-                        break;
-                    }
-                    s.pop();
-                    l -= 1;
-                    s[l] += 1;
-                }
-            }
+        while (digits.len() as i32) < adjusted_limit_position {
+            digits.push(digit_to_u8(0));
         }
     }
     let mut use_e_notation = (e > config.upper_e_break as i32 || e <= config.lower_e_break as i32 || config.force_e_notation) && !config.force_no_e_notation;
     if let Some(max_width) = config.max_width {
+        // Check if it is needed to force using e notation for max width
         let max_width = if sign { max_width - 1 } else { max_width };
         // Is it impossible to represent the value without e notation?
         if e > 0 && e + if config.add_point_zero { 2 } else { 0 } > max_width as i32 {
@@ -341,35 +345,39 @@ fn digits_to_a(sign: bool, mut s: Vec<u8>, mut e: i32, config: FmtFloatConfig) -
         } else if -e + 3 > max_width as i32 {
             use_e_notation = true;
         } else if !use_e_notation {
-            let is_integer = e > s.len() as i32;
+            // Otherwise, prepare to not use e notation
+            let is_integer = e > digits.len() as i32;
             let extra_length = if config.add_point_zero && is_integer { 2 } else { 0 }
                              + if !is_integer && !(e > 0 && e as u8 == max_width) { 1 } else { 0 }
-                             + if e > 0 && s.len() < e as usize { e - s.len() as i32 } else { 0 }
+                             + if e > 0 && digits.len() < e as usize { e - digits.len() as i32 } else { 0 }
                              + if e <= 0 { -e + 1 } else { 0 };
-            let total_length = s.len() + extra_length as usize;
+            let total_length = digits.len() + extra_length as usize;
             if total_length > max_width as usize {
-                let final_char = s.drain((max_width as usize - extra_length as usize)..).nth(0).unwrap();
+                let final_char = digits.drain((max_width as usize - extra_length as usize)..).nth(0).unwrap();
                 if config.round_mode == RoundMode::Round && final_char >= digit_to_u8(5) {
                     // round up
-                    let mut l = s.len() - 1;
-                    s[l] += 1;
-                    while s[l] == digit_to_u8(10) {
+                    let mut l = digits.len() - 1;
+                    digits[l] += 1;
+                    while digits[l] == digit_to_u8(10) {
                         if l == 0 {
-                            s[0] = digit_to_u8(1);
+                            digits[0] = digit_to_u8(1);
                             e += 1;
                             break;
                         }
-                        s.pop();
+                        digits.pop();
                         l -= 1;
-                        s[l] += 1;
+                        digits[l] += 1;
                     }
                 }
             }
         }
     }
+    // Final formatting stage
     if use_e_notation {
         if let Some(max_width) = config.max_width {
-            let mut tail_as_str: String = s.drain(1..).map(|val| val as char).collect();
+            // Very special case: can't include a decimal point
+            // within max_width
+            let mut tail_as_str: String = digits.drain(1..).map(|val| val as char).collect();
             let e_length = format!("{}", e - 1).len();
             let extra_length = 3 + e_length + if sign { 1 } else { 0 };
             if extra_length >= max_width as usize {
@@ -379,23 +387,24 @@ fn digits_to_a(sign: bool, mut s: Vec<u8>, mut e: i32, config: FmtFloatConfig) -
             }
             if tail_as_str.len() == 0 && max_width == 7 && sign {
                 return format!("-{}{}{}",
-                               s[0] as char,
+                               digits[0] as char,
                                if config.capitalize_e { "E" } else { "e" },
                                e - 1);
             }
         }
-        let mut res = String::with_capacity(s.len() + 5);
+        // Generic e-notation case
+        let mut res = String::with_capacity(digits.len() + 5);
         if sign {
             res.push('-');
         }
-        res.push(s[0] as char);
+        res.push(digits[0] as char);
         res.push(config.radix_point);
-        if s.len() == 1 {
+        if digits.len() == 1 {
             if !config.max_width.is_some() {
                 res.push('0');
             }
         } else {
-            for c in &s[1..] {
+            for c in &digits[1..] {
                 res.push(*c as char);
             }
         }
@@ -407,7 +416,8 @@ fn digits_to_a(sign: bool, mut s: Vec<u8>, mut e: i32, config: FmtFloatConfig) -
         res.push_str(format!("{}", e - 1).as_ref());
         return res;
     }
-    let mut as_str = String::with_capacity(s.len() + 3);
+    // Non-e-notation case
+    let mut as_str = String::with_capacity(digits.len() + 3);
     if sign {
         as_str.push('-');
     }
@@ -419,7 +429,7 @@ fn digits_to_a(sign: bool, mut s: Vec<u8>, mut e: i32, config: FmtFloatConfig) -
             as_str.push('0');
         }
     }
-    for digit in s {
+    for digit in digits {
         if e > 0 && curr == e {
             as_str.push(config.radix_point);
         }
@@ -497,12 +507,26 @@ mod tests {
 
     #[test]
     fn test_widths() {
+        // Test random floats with several configurations, and
+        // make sure that .max_width(_) does its job
         let mut rng = rand::thread_rng();
+
+        let configs = &[
+            FmtFloatConfig::default(),
+            FmtFloatConfig::default()
+                .force_no_e_notation(),
+            FmtFloatConfig::default()
+                .add_point_zero(true),
+            FmtFloatConfig::default()
+                .truncate()
+                .force_e_notation()
+                .min_significant_digits(5),
+        ];
         
         for width in 6..=13 {
-            let config = FmtFloatConfig::default()
-                .max_width(width);
-            for _ in 0..10000 {
+            for i in 0..20000 {
+                let config = configs[i % configs.len()]
+                    .max_width(width);
                 let val = f64::from_bits(rng.gen::<u64>());
                 if val > 0.0 && val <= 9e-99 && width == 6 {
                     // unrepresentable
@@ -527,6 +551,8 @@ mod tests {
 
     #[test]
     fn test_round_trip_dtoa() {
+        // Make sure random floats with several configurations
+        // round trip perfectly
         let mut rng = rand::thread_rng();
         
         let configs = &[
@@ -552,6 +578,7 @@ mod tests {
     
     #[test]
     fn test_round_trip_ftoa() {
+        // Test round tripping for random f32's
         let mut rng = rand::thread_rng();
         
         let configs = &[
